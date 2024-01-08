472df87cd8898ccfccd65dc6a9957c3a70ac1fb4
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-08 17:03:04 +0100

    Handle admin login

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -4,18 +4,18 @@
         <h2 class="text-2xl font-semibold mb-4">Create An Account</h2>
         <p class="text-gray-600 mb-6">Please fill out this form to register with us</p>
 
-        <form action="<?php echo URLROOT; ?>/users/login" method="post">
+        <form id="login-form">
             <div class="mb-4">
                 <label for="email" class="block text-gray-600">Email: *</label>
                 <input type="email" name="email" value="<?php echo $data['email'];?>" 
                     class="mt-1 p-2 w-full border rounded-md">
-                <span class="text-red-500"> <?php echo $data['email_err']; ?> </span>
+                <span id="login-email-err" class="text-red-500"> <?php echo $data['email_err']; ?> </span>
             </div>
             <div class="mb-4">
                 <label for="password" class="block text-gray-600">Password: *</label>
                 <input type="password" name="password" value="<?php echo $data['password'];?>" 
                     class="mt-1 p-2 w-full border rounded-md">
-                <span class="text-red-500"> <?php echo $data['password_err']; ?> </span>
+                <span id="login-password-err" class="text-red-500"> <?php echo $data['password_err']; ?> </span>
             </div>
 
             <button type="submit" value="Login"
