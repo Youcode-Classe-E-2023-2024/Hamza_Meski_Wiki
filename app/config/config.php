1b71f80e278434dee0727f410c2edf9ff993b6fa
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-08 12:24:49 +0100

    register and login
    register and login handling

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php 
+// database params 
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'wiki');
+
+// app root 
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL root 
+define('URLROOT', 'http://localhost/Hamza_Meski_Wiki');
+// site name
+define('SITENAME', 'Wiki');
+// app version 
+define('APPVERSION', '1.0.0');
+?>
