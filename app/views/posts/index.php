1b4a6753f1de376ef12f91ffaae593fb6a489698
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-10 00:24:44 +0100

    crud

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -1,15 +1,20 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-<div id="posts-index"></div>
-    <table id="postsTable" class="min-w-full bg-white border border-gray-300 shadow-md rounded-md overflow-hidden">
+<div id="posts-index" class="container mx-auto my-8 p-6 bg-white border border-gray-300 shadow-md rounded-md overflow-hidden">
+    <a href="" class="bg-blue-500">Add Post</a>
+    <table id="postsTable" class="">
         <thead class="bg-gray-200 text-gray-700">
             <tr>
-                <th class="py-2 px-4">Id</th>
-                <th class="py-2 px-4">Title</th>
-                <th class="py-2 px-4">Content</th>
-                <th class="py-2 px-4">Image</th>
-                <th class="py-2 px-4">Action</th>
+                <th class="py-3 px-4">Id</th>
+                <th class="py-3 px-4">Title</th>
+                <th class="py-3 px-4">Content</th>
+                <th class="py-3 px-4">Image</th>
+                <th class="py-3 px-4">Action</th>
             </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+            <!-- Your table rows go here -->
+        </tbody>
     </table>
+
+</div>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
