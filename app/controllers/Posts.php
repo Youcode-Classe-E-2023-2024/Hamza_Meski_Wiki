3abb89f1f2afb2622f275054adf0e5c3b19bbc79
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-09 23:55:51 +0100

    Handling error

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,17 +1,22 @@
 <?php 
 class Posts extends Controller {
     public $postModel;
-    public function index() {
+
+    public function __construct() {
         $this->postModel = $this->model('Post');
+    }
+
+    public function index() {
         $this->view('Posts/index');
     }
 
     // public
     public function getPostsByUserId() {
         $userId = $_SESSION['user_id']; 
-        echo $userId;
-        die();
-        $this->postModel->getPostsByUserId($userId);
+        $posts = $this->postModel->getPostsByUserId($userId);
+        echo '<pre>'; 
+        print_r($posts); 
+        echo '</pre>';
     }
 }
 ?>
