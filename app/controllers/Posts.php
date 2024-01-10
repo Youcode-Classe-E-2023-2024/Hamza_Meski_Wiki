26e0ddccc9f9bdbf1b493f75ea49631850ea7851
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-10 12:22:52 +0100

    Handling errors

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -26,17 +26,6 @@ class Posts extends Controller {
         echo json_encode($posts);
     }
 
-    public function addPostSection() {
-        $categories = $this->categoryModel->getCategories();
-        $tags = $this->tagModel->getTags();
-        $data = [
-            'user_id' => $_SESSION['user_id'],
-            'categories' => $categories, 
-            'tags' => $tags
-        ];
-        $this->view('posts/addPostSection', $data);
-    }
-
     public function addPost() {
 
         $this->postModel->addPost($_SESSION['user_id'] );
