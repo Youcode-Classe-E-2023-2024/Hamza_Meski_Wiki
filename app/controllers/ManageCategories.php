8589a5f9d442d6862b980aea5777e43c8712d215
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-11 18:59:16 +0100

    category controller

--- app/controllers/ManageCategories.php
+++ app/controllers/ManageCategories.php
@@ -11,7 +11,6 @@ class ManageCategories extends Controller {
     }
 
     public function addCategory(){
-
         // sanitize POST array
         $category_name = filter_var($_POST['category_name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
@@ -39,6 +38,37 @@ class ManageCategories extends Controller {
         ];
 
         $this->categoryModel->addCategory($data);
+    }
+
+    public function updateCategory(){
+        // sanitize POST array
+        $category_id = filter_var($_POST['category_id'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $category_name = filter_var($_POST['category_name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+        // Handling the image upload 
+        if (isset($_FILES['image_name']) && $_FILES['image_name']['error'] === UPLOAD_ERR_OK) {
+            // Get the file name and generate a unique filename
+            $imageName = uniqid() . '_' . basename($_FILES['image_name']['name']);
+    
+            // Set the local path where the image will be saved
+            $imagePath = PROJECT_ROOT . '/public/images/' . $imageName;
+    
+            // Move the uploaded file to the specified path
+            move_uploaded_file($_FILES['image_name']['tmp_name'], $imagePath);
+    
+            // Update the $image_name variable with the local path
+            $image_name = $imageName;
+        } else {
+            // If no file is uploaded, use a default image name
+            $image_name = 'ice.avif';
+        }
+        
+        $data = [
+            'category_name' => trim($category_name), 
+            'image_name' => trim($image_name)
+        ];
+
+        $this->categoryModel->updateCategory($category_id, $data);
     }
 
 }
