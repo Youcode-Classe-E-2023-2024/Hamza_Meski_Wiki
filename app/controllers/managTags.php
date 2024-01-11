8e36adc1af6156d3a8cedd2683742f284dd14f8a
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-11 22:13:40 +0100

    mamage Tags controller fixing

--- app/controllers/managTags.php
+++ app/controllers/managTags.php
@@ -13,79 +13,36 @@ class ManageTags extends Controller {
     public function addTag(){
 
         // sanitize POST array
-        $category_name = filter_var($_POST['category_name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-
-        // Handling the image upload 
-        if (isset($_FILES['image_name']) && $_FILES['image_name']['error'] === UPLOAD_ERR_OK) {
-            // Get the file name and generate a unique filename
-            $imageName = uniqid() . '_' . basename($_FILES['image_name']['name']);
-    
-            // Set the local path where the image will be saved
-            $imagePath = PROJECT_ROOT . '/public/images/' . $imageName;
-    
-            // Move the uploaded file to the specified path
-            move_uploaded_file($_FILES['image_name']['tmp_name'], $imagePath);
-    
-            // Update the $image_name variable with the local path
-            $image_name = $imageName;
-        } else {
-            // If no file is uploaded, use a default image name
-            $image_name = 'ice.avif';
-        }
+        $tag_name = filter_var($_POST['tag_name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         
         $data = [
-            'category_name' => trim($category_name), 
-            'image_name' => trim($image_name)
+            'tag_name' => trim($tag_name)
         ];
     
-        $this->categoryModel->addCategory($data);
+        $this->tagModel->addCategory($data);
     }
 
     public function updateTag(){
         // sanitize POST array
-        $category_id = filter_var($_POST['category_id'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $category_name = filter_var($_POST['category_name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-
-        // Handling the image upload 
-        if (isset($_FILES['image_name']) && $_FILES['image_name']['error'] === UPLOAD_ERR_OK) {
-            // Get the file name and generate a unique filename
-            $imageName = uniqid() . '_' . basename($_FILES['image_name']['name']);
-    
-            // Set the local path where the image will be saved
-            $imagePath = PROJECT_ROOT . '/public/images/' . $imageName;
-    
-            // Move the uploaded file to the specified path
-            move_uploaded_file($_FILES['image_name']['tmp_name'], $imagePath);
-    
-            // Update the $image_name variable with the local path
-            $image_name = $imageName;
-        } else {
-            // If no file is uploaded, use a default image name
-            $image_name = 'ice.avif';
-        }
+        $tag_id = filter_var($_POST['tag_id'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $tag_name = filter_var($_POST['tag_name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         
         $data = [
-            'category_name' => trim($category_name), 
-            'image_name' => trim($image_name)
+            'tag_name' => trim($tag_name)
         ];
 
-        $this->categoryModel->updateCategory($category_id, $data);
-        echo '<pre>'; 
-        print_r($data); 
-        echo '</pre>';
-        die();
+        $this->tagModel->updateTag($tag_id, $data);
     }
 
-    public function deleteTag($category_id) {
-        echo $category_id;
-        $this->categoryModel->deleteCategory($category_id);
+    public function deleteTag($tag_id) {
+        $this->tagModel->deleteCategory($tag_id);
     }
 
     public function getTags() {
-        echo json_encode($this->categoryModel->getCategories());
+        echo json_encode($this->tagModel->getCategories());
     }
 
     public function getTagById($id) {
-        echo json_encode($this->categoryModel->getCategoryById($id));
+        echo json_encode($this->tagModel->getCategoryById($id));
     }
 }
