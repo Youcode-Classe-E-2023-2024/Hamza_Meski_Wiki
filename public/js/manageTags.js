bf0ffef5647c7ec16ecae3e371f60f76380da00c
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-13 21:13:41 +0100

    Improving UI

--- public/js/manageTags.js
+++ public/js/manageTags.js
@@ -55,11 +55,10 @@ if(manageTagsIndex){
                     "data": "id",
                     "render": function(data, type, row) {
                         if (type === 'display') {
-                            return `<div class="flex justify-center">
+                            return `<div class="flex items-center justify-center">
                                         <span class="bg-green-500 h-10 w-10 p-2 rounded-full text-center text-white">${data}</span>
                                     </div>`;
                         }
-            
                         return data;
                     }
                 },
@@ -67,17 +66,16 @@ if(manageTagsIndex){
                     "data": "name",
                     "render": function(data, type, row) {
                         if (type === 'display') {
-                            return `<div class="flex justify-center bg-gray-500 p-4 text-white">
+                            return `<div class="text-center font-bold text-green-700 bg-green-100 p-2 rounded">
                                         <strong>${data}</strong>
                                     </div>`;
                         }
-            
                         return data;
                     }
                 },
                 {
-                    data: 'id',
-                    render: function(data) {
+                    "data": 'id',
+                    "render": function(data) {
                         return `<div class="flex space-x-2 justify-center">
                                     <button onclick="deleteTag(${data})" name="btn" class="bg-red-500 p-2 rounded-md text-white hover:bg-red-600">Delete</button>
                                     <button data-modal-target="edit-tag" data-modal-toggle="edit-tag" onclick="tagIdAgent(${data})" data-modal-target="edit-modal" data-modal-toggle="edit-modal" onclick="displayEditModel(${data})" name="btn" class="bg-blue-500 p-2 rounded-md text-white hover:bg-blue-600">Update</button>
@@ -86,10 +84,10 @@ if(manageTagsIndex){
                 }
             ],
             "createdRow": function(row, data, dataIndex) {
-                $(row).addClass('hover:bg-gray-100 bg-black');
+                $(row).addClass('hover:bg-gray-100');
             },
-            
         });
     });
+    
     
 }
