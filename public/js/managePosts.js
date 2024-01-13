837f2252f5135cebc9d5425d753c1faaa2bf3db2
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-01-13 15:34:29 +0100

    length in DT js

--- public/js/managePosts.js
+++ public/js/managePosts.js
@@ -58,7 +58,19 @@ if(postsIndex){
             "columns": [
                 {"data": "id"},
                 {"data": "title"},
-                {"data": "content"},
+                {
+                    "data": "content",
+                    "render": function (data) {
+                        // Set the maximum length for the content
+                        var maxLength = 60;
+    
+                        // Truncate the content if it exceeds the maximum length
+                        var truncated = data.length > maxLength ? data.substr(0, maxLength) + '...' : data;
+    
+                        // Return the truncated content
+                        return truncated;
+                    }
+                },
                 {"data": "image_name"},
                 {
                     data: 'id',
